2e71df3744f4984f2d426b4faffa243d6c585e05
Krzysztof Wszoła <krzyzak864@gmail.com>
2025-11-12 16:57:41 +0100

    p7

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -5,21 +5,36 @@
 class ResourceManager
 {
 public:
+    
     ResourceManager()
-    { 
-        
+    {   
+        Obj = new Resource;
     }
     ResourceManager(const ResourceManager& kopia)
     { 
-        object = kopia.object;
+        //object = kopia.object;
     }
     ResourceManager& operator=(const ResourceManager& przypisany)
     {
-
+        
         return *this;
     }
+    /* ResourceManager(ResourceManager&& przeniesiony)
+    { 
+        ResourceManager zwrot{std::move(przeniesiony)};
+        return zwrot;
+    } */
+     ~ResourceManager()
+    {
+        delete Obj;
+    }
 
-    double get() { return object.get();}
+    double get()
+    {
+        return Obj->get();
+    } 
 
-    Resource object;
+
+    Resource* Obj;
+    // tu musi byæ dynamiczna alokacja
 };
